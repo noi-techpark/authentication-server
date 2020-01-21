4711a17366fe86544915adc227cae3499beb464d
Alex Lanz <alex@krumer.it>
2020-01-21 13:37:35 +0100

    Production installation optimizations

--- docker/keycloak/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eou pipefail
-
-echo "set keycloak_private_address=${JGROUPS_PRIVATE_ADDRESS:-127.0.0.1}" >> "$JBOSS_HOME/bin/.jbossclirc"
-
-exec /opt/jboss/tools/docker-entrypoint.sh $@
-exit $?
