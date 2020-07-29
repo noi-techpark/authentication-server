7dc253b9718b099234b91055ddd6a05e01846022
Alex Lanz <alex@krumer.it>
2020-07-29 11:32:06 +0200

    Fixing CI

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -21,14 +21,23 @@ pipeline {
         }
         stage('Assets') {
             steps {
-                sh 'docker run --rm -v $PWD:/code -w /code/themes/noi/common/resources -u $(id -u jenkins):$(id -g jenkins) node:12 npm install'
-                sh 'docker run --rm -v $PWD:/code -w /code/themes/noi/common/resources -u $(id -u jenkins):$(id -g jenkins) node:12 npm run build'
+                sh """
+                    docker build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins) -t ${DOCKER_IMAGE}-node:${BUILD_NUMBER} infrastructure/docker/node
+
+                    docker run --rm -v $PWD:/code -w /code/themes/noi/common/resources ${DOCKER_IMAGE}-node:${BUILD_NUMBER} npm install
+                    docker run --rm -v $PWD:/code -w /code/themes/noi/common/resources ${DOCKER_IMAGE}-node:${BUILD_NUMBER} npm run build
+                """
             }
         }
         stage('Build') {
             steps {
                 sh "docker-compose -f infrastrucutre/docker-compose.build.yml build"
             }
+        }
+    }
+    post {
+        always {
+            sh "docker image rm -f ${DOCKER_IMAGE}-node:${BUILD_NUMBER}"
         }
     }
 }
