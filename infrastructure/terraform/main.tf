f41d014473104a9c7f255d51ccd9f221a2b79bed
Alex Lanz <alex.lanz@aboutbits.it>
2022-08-29 14:55:50 +0200

    fix Terraform provider declaration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 2.0"
   region = "eu-west-1"
 }
 
